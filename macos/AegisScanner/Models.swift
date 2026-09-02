d673463ef59bb9909468e0cb265e0c21932e4ce3
bpms9cmnxc-debug <bpms9cmnxc@privaterelay.appleid.com>
2026-09-02 08:54:42 +0200

    2.1.5: Versionsstempel

--- macos/AegisScanner/Models.swift
+++ macos/AegisScanner/Models.swift
@@ -2,9 +2,9 @@ import CoreGraphics
 import Foundation
 
 enum AppVersion {
-    static let marketing = "2.1.4"
+    static let marketing = "2.1.5"
     static let channel = "alpha"
-    static let display = "2.1.4 alpha"
+    static let display = "2.1.5 alpha"
 }
 
 enum StrategyTrack: String, CaseIterable, Identifiable {
